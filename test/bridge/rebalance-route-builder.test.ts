5afcd4f6c7c62a3de92f33246b31bd6267e35a97
dvilela <dvilelaf@gmail.com>
2026-07-08 04:50:34 +0200

    Use Solana zero caller in CCTP test

--- test/bridge/rebalance-route-builder.test.ts
+++ test/bridge/rebalance-route-builder.test.ts
@@ -1575,7 +1575,7 @@ function cctpIrisMessage(_burnTransactionHash: string, mintRecipient: string = W
           destinationDomain: '3',
           sender: CCTP_TOKEN_MESSENGER,
           recipient: CCTP_TOKEN_MESSENGER,
-          destinationCaller: '11111111111111111111111111111111',
+          destinationCaller: utils.hexZeroPad('0x', 32),
           decodedMessageBody: {
             burnToken: BASE_USDC,
             mintRecipient,
@@ -1602,7 +1602,7 @@ function cctpSolanaIrisMessage(_burnTransactionHash: string, mintRecipient: stri
           destinationDomain: '5',
           sender: CCTP_TOKEN_MESSENGER,
           recipient: SOLANA_CCTP_TOKEN_MESSENGER,
-          destinationCaller: utils.hexZeroPad('0x', 32),
+          destinationCaller: '11111111111111111111111111111111',
           decodedMessageBody: {
             burnToken: BASE_USDC,
             mintRecipient,
